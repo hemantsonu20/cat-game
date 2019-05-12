e8b48253233a34476a2f8c9d5edb73d0e3a39830
Pratapi Hemant Patel <hemantsonu20@gmail.com>
2019-05-12 23:17:24 +0530

    docs added

---
+++ docs/precache-manifest.1fc3d013bd16769ea81094e1a7bc26f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fd218b1536e6cfb796d4bcee4631deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f55738d4ba639653929b",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4bff071e0a52dc932c43",
+    "url": "/static/css/main.2868dda5.chunk.css"
+  },
+  {
+    "revision": "f55738d4ba639653929b",
+    "url": "/static/js/2.61b6dae8.chunk.js"
+  },
+  {
+    "revision": "4bff071e0a52dc932c43",
+    "url": "/static/js/main.88db135d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Game from './components/Game';
-import './styles/styles.css';
 
 function App() {
   return (
